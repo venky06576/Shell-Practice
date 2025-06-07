a9616ab7ea9444819bf0cbced2d555c404ced5df
venkatesh <venky06576@gmail.com>
2025-06-07 22:04:15 +0530

    Comment Changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-PERSON1=$PERSON1;
-PERSSON2=$PERSSON2;
+PERSON1=trump;
+PERSSON2=Musk;
 
-echo "PERSON1: Hello PERSON2 ,How are you"
-echo "PERSON2 : I am good how are you"
-echo "PERSON1: can we disccuss about the business"
-echo "PERSON2: can we meet tonight to discuss "
+echo "$PERSON1: Hello PERSON2 ,How are you"
+echo "$PERSON2 : I am good how are you"
+echo "$PERSON1: can we disccuss about the business"
+echo "$PERSON2: can we meet tonight to discuss "
