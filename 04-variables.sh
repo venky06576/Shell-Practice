0da58bc4523d0836e065e14bdedfdc43114ab6cd
venkatesh <venky06576@gmail.com>
2025-06-07 22:22:06 +0530

    Comment Changes

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "PERSON1: Hello PERSON2 ,How are you"
-echo "PERSON2 : I am good how are you"
-echo "PERSON1: can we disccuss about the business"
-echo "PERSON2: can we meet tonight to discuss "
+echo "$PERSON1: Hello PERSON2 ,How are you"
+echo "$PERSON2 : I am good how are you"
+echo "$PERSON1: can we disccuss about the business"
+echo "$PERSON2: can we meet tonight to discuss "
