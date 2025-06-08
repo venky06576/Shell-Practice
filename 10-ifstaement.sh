e07dde164e971d15fd60b70ef1f31f66063feb98
venkatesh <venky06576@gmail.com>
2025-06-08 11:08:00 +0530

    Comment Changes

--- 10-ifstaement.sh
+++ 10-ifstaement.sh
@@ -4,8 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10  ]
 then
-    echo "Given $NUMBER is less than 10"
+    echo "Given number  $NUMBER is less than 10"
 else
-    echo "Given $NUMBER is not less tha 10" 
+    echo "Given number $NUMBER is not less tha 10" 
 fi    
  
