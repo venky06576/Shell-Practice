ece78b5fe07b196edce064edc9a8e3419f31ef2a
venkatesh <venky06576@gmail.com>
2025-06-08 10:28:36 +0530

    Comment Changes

--- 09-spcialvariable.sh
+++ 09-spcialvariable.sh
@@ -4,3 +4,12 @@ echo "Hello from Venkatesh"
 echo "Name of shell script = $0"
 
 echo "Number of arguments passed to the script =$#"
+
+echo "Script Name = $0"
+echo "Current Directory =$PWD"
+echo "user running the script =$USER"
+echo "Home Directory of the script =$HOME"
+echo "PID of the Script =$$"
+
+sleep 10 &
+echo "PID of the last command in backgroung : $!"
