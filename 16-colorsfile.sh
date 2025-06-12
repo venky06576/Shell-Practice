0ca3ada0998f5ea13fc48293c16b5655409396ac
venkatesh <venky06576@gmail.com>
2025-06-12 17:20:21 +0530

    function program

--- 16-colorsfile.sh
+++ 16-colorsfile.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-USERID =$(id-u)
-
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
-if [ USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
-    echo "$R ERROR:: Please run the script witht access $N"
-    exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 #give other than 0 upto 127
 else
-    echo "You are Running with root access"
-
+    echo "You are running with root access"
 fi
