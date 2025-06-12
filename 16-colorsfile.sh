c2811e12a9ed86a5db5708f4743e536336c4a32d
venkatesh <venky06576@gmail.com>
2025-06-12 17:29:12 +0530

    function program

--- 16-colorsfile.sh
+++ 16-colorsfile.sh
@@ -13,3 +13,47 @@ then
 else
     echo "You are running with root access"
 fi
+
+VALIDATE(){
+
+if [ $! eq 0]
+then 
+    echo -e "Installing $2 is .....$G success $N"
+else
+     echo -e "Installing $2 is .....$G Failue $N"
+     exit 1
+fi
+}
+
+dnf list installed mysql 
+
+if [ $! -ne 0 ]
+then
+    echo "MYSQL is not installed .. going to install"
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo -e "Nothing to do mysql ....$Y already installed $N"
+
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e "Nothing to do python... $Y already installed $N"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo -e "Nothing to do nginx... $Y already installed $N"
+fi
+   
